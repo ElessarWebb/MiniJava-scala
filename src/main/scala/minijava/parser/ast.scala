47f12b2f11ab741b4c3819e31966fdfc50f33c81
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2013-11-29 20:43:55 +0100

    Parser now handles statements

--- src/main/scala/minijava/parser/ast.scala
+++ src/main/scala/minijava/parser/ast.scala
@@ -1,89 +1,114 @@
-package minijava.parser
+package minijava {
 
-sealed abstract class Term
-sealed abstract class Statement extends Term
-sealed abstract class Exp extends Term
-sealed abstract class Type extends Term
+package object parser {
+	type Field = VarDecl
+}
 
-sealed abstract class BinOp extends Term
-case object Plus extends BinOp
-case object Minus extends BinOp
-case object Mul extends BinOp
-case object LAnd extends BinOp
-case object Lt extends BinOp
-case object Subscript extends BinOp
+package parser {
+	sealed abstract class Term
+	sealed abstract class Statement extends Term
+	sealed abstract class Exp extends Term
+	sealed abstract class Type extends Term
 
-sealed abstract class UnOp extends Term
-case object Neg extends UnOp
-case object Len extends UnOp
+	sealed abstract class BinOp extends Term
+	case object Plus extends BinOp
+	case object Minus extends BinOp
+	case object Mul extends BinOp
+	case object LAnd extends BinOp
+	case object Lt extends BinOp
+	case object Subscript extends BinOp
 
-case object TBool extends Type
-case object TInt extends Type
-case object TIntArray extends Type
-case class TClass(id: String) extends Type
+	sealed abstract class UnOp extends Term
+	case object Neg extends UnOp
+	case object Len extends UnOp
 
-// more literals
-case object True extends Exp
-case object False extends Exp
-case class BinExp( op: BinOp, left: Exp, right: Exp ) extends Exp
-case class UnExp( op: UnOp, right: Exp ) extends Exp
+	case object TBool extends Type
+	case object TInt extends Type
+	case object TIntArray extends Type
+	case class TClass(id: String) extends Type
 
-// statements
-case class IfStmt(
-	cond: Exp,
-	then_body: Statement,
-	else_body: Statement
-	) extends Statement
+	// more literals
+	case object True extends Exp
+	case object False extends Exp
+	case class BinExp( op: BinOp, left: Exp, right: Exp ) extends Exp
+	case class UnExp( op: UnOp, right: Exp ) extends Exp
 
-case class BlockStmt(
-	body: List[Statement]
-	) extends Statement
+	// statements
+	case class IfStmt(
+		cond: Exp,
+		then_body: Statement,
+		else_body: Statement
+		) extends Statement
 
-// class definitions
-case class MainClass(
-	id: String,
-	argv: String,
-	body: Statement
-	) extends Term
+	case class WhileStmt(
+		cond: Exp,
+		body: Statement
+		) extends Statement
 
-case class ClassDecl(
-	id: String,
-	parent: Option[String],
-	fields: List[VarDecl],
-	methods: List[MethodDecl]
-	) extends Term
+	case class BlockStmt(
+		body: List[Statement]
+		) extends Statement
 
-case class VarDecl(
-	vtype: Type,
-	id: String
-	) extends Term
+	case class PrintStmt(
+		exp: Exp
+		) extends Statement
 
-// alias for fields
-type Field = VarDecl
+	case class AssignStmt(
+		id: String,
+		value: Exp
+		) extends Statement
 
-case class Param(
-	vtype: Type,
-	id: String
-	) extends Term
+	case class ArrayAssignStmt(
+		id: String,
+		index: Exp,
+		value: Exp
+		) extends Statement
 
-case class MethodDecl(
-	mtype: Type,
-	id: String,
-	params: List[Param],
-	vars: List[VarDecl],
-	body: List[Statement],
-	ret_exp: Exp
-	) extends Term
+	// class definitions
+	case class MainClass(
+		id: String,
+		argv: String,
+		body: Statement
+		) extends Term
 
-// statements
-case class Print(
-	exp: Exp
-	) extends Statement
+	case class ClassDecl(
+		id: String,
+		parent: Option[String],
+		fields: List[VarDecl],
+		methods: List[MethodDecl]
+		) extends Term
 
-case class Assign(
-	vid: String,
-	exp: Exp
-	)
+	case class VarDecl(
+		vtype: Type,
+		id: String
+		) extends Term
 
-// expressions
-case class IntValue( value: Int ) extends Exp
+	case class Param(
+		vtype: Type,
+		id: String
+		) extends Term
+
+	case class MethodDecl(
+		mtype: Type,
+		id: String,
+		params: List[Param],
+		vars: List[VarDecl],
+		body: List[Statement],
+		ret_exp: Exp
+		) extends Term
+
+	// statements
+	case class Print(
+		exp: Exp
+		) extends Statement
+
+	case class Assign(
+		vid: String,
+		exp: Exp
+		)
+
+	// expressions
+	case class IntValue( value: Int ) extends Exp
+
+	} // end of parser package
+} // end of minijava package
--- src/main/scala/minijava/parser/parser.scala
+++ src/main/scala/minijava/parser/parser.scala
@@ -3,9 +3,48 @@ package minijava.parser
 import scala.util.parsing.combinator._
 
 /**
- * Op precedence (high to low): !, *, (+,-), <, &&
+ * Defines parsers for all minijava terminals
  */
-trait ExpressionParsers extends MiniJavaLiteralParsers {
+trait MJLiteralParsers extends RegexParsers {
+	// literals
+	def int: Parser[Int] = "[-]?[0-9]+".r ^^ { _.toInt }
+	def id: Parser[String] = not( reserved ) ~> "[a-zA-Z][a-zA-Z0-9_]*".r
+
+	// some keywords
+	def class_kw: Parser[String] = "class"
+	def extends_kw: Parser[String] = "extends"
+	def public_kw: Parser[String] = "public"
+	def return_kw: Parser[String] = "return"
+	def while_kw: Parser[String] = "while"
+	def if_kw: Parser[String] = "if"
+	def else_kw: Parser[String] = "else"
+	def true_kw: Parser[String] = "true"
+	def false_kw: Parser[String] = "false"
+	def println_kw = "System" ~ "." ~ "out" ~ "." ~ "println"
+
+	//
+	def reserved = {
+		class_kw |
+		extends_kw |
+		public_kw |
+		return_kw |
+		while_kw |
+		if_kw |
+		else_kw |
+		true_kw |
+		false_kw
+	}
+}
+
+object ExpressionParser extends MJExpParsers {
+	def apply( input: String ) = parseAll( exp, input )
+}
+
+/**
+ * Parses a minijava expression
+ * Operator precedence (high to low): !, *, (+,-), <, &&
+ */
+trait MJExpParsers extends MJLiteralParsers {
 	def intval = int ^^ { x => IntValue( x ) }
 	def boolval = ( true_kw | false_kw ) ^^ {
 		case "true" => True
@@ -23,12 +62,10 @@ trait ExpressionParsers extends MiniJavaLiteralParsers {
 	def not_term: Parser[Exp] = op_not ~> simpleterm ^^ { x => UnExp( Neg, x ) }
 	def term: Parser[Exp] = simpleterm | not_term | ( "(" ~> exp <~ ")" )
 
-	// convert a series of log. ands to a nested and expression
 	def exp_mul = term ~ rep( op_mul ~> term ) ^^ {
 		case left ~ rights => rights.foldLeft( left ) { ( acc, r ) => BinExp( Mul, acc, r )}
 	}
 
-	// convert a series of additions/subtractions to the appropriate binary expressions
 	def exp_sum: Parser[Exp] = exp_mul ~ rep(( op_plus | op_minus ) ~ exp_mul ) ^^ {
 		case left ~ rights => rights.foldLeft( left ) { ( acc, r ) =>
 			r match {
@@ -38,13 +75,11 @@ trait ExpressionParsers extends MiniJavaLiteralParsers {
 	}
 
 	// a < b < c is not supported
-	// convert a < b to BinExp
 	def exp_lt: Parser[Exp] = exp_sum ~ ( op_lt ~> exp_sum ).? ^^ {
 		case left ~ Some(right) => BinExp( Lt, left, right )
 		case left ~ None => left
 	}
 
-	// convert a && b && ...
 	def exp_land = exp_lt ~ rep( op_and ~> exp_lt ) ^^ {
 		case left ~ rights => rights.foldLeft( left ) { ( acc, r ) => BinExp( LAnd, acc, r )}
 	}
@@ -52,44 +87,42 @@ trait ExpressionParsers extends MiniJavaLiteralParsers {
 	def exp:Parser[Exp] = exp_land
 }
 
-object ExpressionParser extends parser.ExpressionParsers {
-	def apply( input: String ) = parseAll( exp, input )
-}
-
-trait MiniJavaParsers extends MiniJavaLiteralParsers with ExpressionParsers {
+trait MJParsers extends MJLiteralParsers with MJExpParsers {
 
 	// goal
 	def program = mainclass ~ rep(classdecl)
 
-	// main class definition
 	def typ: Parser[Type] = {
-		"int" ^^ { _ => TInt } |
-			"boolean" ^^ { _ => TBool } |
 			"int" ~ "[" ~ "]" ^^ { _ => TIntArray } |
+			"int" ^^ { _ => TInt } |
+			"boolean" ^^ { _ => TBool } |
 			id ^^ { x => TClass(x) }
 	}
 
-	def decl = typ ~ id
+	/**
+	 * Parse generic declaration
+	 * used for fields/vardefs/params
+	 */
+	private def decl = typ ~ id
 
 	def field: Parser[Field] = decl <~ ";" ^^ {
 		case t ~ id => VarDecl( t, id )
 	}
 
-	def var_decl = decl <~ ";" ^^ { case t ~ i => VarDecl( t, i ) }
+	def vardecl = decl <~ ";" ^^ { case t ~ i => VarDecl( t, i ) }
 
-	def statement = id <~ ";" ^^ { _ => BlockStmt( List[Statement]() ) }
 	def param: Parser[Param] = decl ^^ {
 		case t ~ id => Param( t, id )
 	}
 
-	def method = {
+	def method: Parser[MethodDecl] = {
 		public_kw ~> typ ~ id ~ ( "(" ~> rep( param ) <~ ")" ) ~
-			( "{" ~> rep( var_decl ) ~ rep( statement ) ~ ( return_kw ~> exp <~ ";" ) <~ "}" ) ^^ {
+			( "{" ~> rep( vardecl ) ~ rep( statement ) ~ ( return_kw ~> exp <~ ";" ) <~ "}" ) ^^ {
 			case ( t ~ i ) ~ ps ~ ( ds ~ ss ~ r ) => MethodDecl( t, i, ps, ds, ss, r )
 		}
 	}
 
-	def classdecl = {
+	def classdecl: Parser[ClassDecl] = {
 		class_kw ~> id ~ ( extends_kw ~> id ).? ~
 			( "{" ~> field.* ~ method.* <~ "}" ) ^^ {
 			case id ~ p ~ ( fs ~ ms ) => ClassDecl( id, p, fs, ms )
@@ -103,30 +136,42 @@ trait MiniJavaParsers extends MiniJavaLiteralParsers with ExpressionParsers {
 		}
 	}
 
-	def mainclass = {
+	def mainclass: Parser[MainClass] = {
 		( "class" ~> id ) ~ ( "{" ~> mainmethod <~  "}" ) ^^ {
 			case c ~ main => MainClass( c, main._1, main._2 )
 		}
 	}
+
+	def statement:Parser[Statement] = {
+		block_stmt |
+		if_stmt |
+		while_stmt |
+		print_stmt |
+		assign_stmt |
+		array_assign_stmt
+	}
+
+	def block_stmt = "{" ~> rep( statement ) <~ "}" ^^ { case x => BlockStmt( x ) }
+
+	def if_stmt = "if" ~> ( "(" ~> exp <~ ")" ) ~ statement ~ ( "else" ~> statement ) ^^ {
+		case ( exp ) ~ thenst ~ ( elsest ) => IfStmt( exp, thenst, elsest )
+	}
+
+	def while_stmt = "while" ~> ( "(" ~> exp <~ ")" ) ~ statement ^^ {
+		case ( exp ) ~ stmts => WhileStmt( exp, stmts )
+	}
+
+	def print_stmt = println_kw ~ "(" ~> exp <~ ")" <~ ";" ^^ { case e => PrintStmt( e ) }
+
+	def assign_stmt = id ~ ( "=" ~> exp ) <~ ";" ^^ { case id ~ ( value ) =>
+		AssignStmt( id, value )
+	}
+
+	def array_assign_stmt = id ~ ( "[" ~> exp <~ "]" ) ~ ( "=" ~> exp ) <~ ";" ^^ { case id ~ ( index ) ~ ( value ) =>
+		ArrayAssignStmt( id, index, value )
+	}
 }
-object MiniJavaParser extends MiniJavaParsers {
+
+object MJParser extends MJParsers {
 	def apply( input: String ) = parseAll( program, input )
-}
-
-trait MiniJavaLiteralParsers extends RegexParsers {
-	// literals
-	def int: Parser[Int] = "[-]?[0-9]+".r ^^ { _.toInt }
-	def id: Parser[String] = not( reserved ) ~> "[a-zA-Z][a-zA-Z0-9_]*".r
-
-	def class_kw: Parser[String] = "class"
-	def extends_kw: Parser[String] = "extends"
-	def public_kw: Parser[String] = "public"
-	def return_kw: Parser[String] = "return"
-	def while_kw: Parser[String] = "while"
-	def if_kw: Parser[String] = "if"
-	def else_kw: Parser[String] = "else"
-	def true_kw: Parser[String] = "true"
-	def false_kw: Parser[String] = "false"
-
-	def reserved = class_kw | extends_kw | public_kw | return_kw | while_kw | if_kw | else_kw | true_kw | false_kw
 }
